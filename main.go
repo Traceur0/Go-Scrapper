00623829a76c57a047295e0bdda3d4addb433353
Traceur0 <ghost01ghost@gmail.com>
2022-04-06 17:26:10 +0900

    append job info to array

--- main.go
+++ main.go
@@ -5,24 +5,35 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var mainURL = "https://www.jobkorea.co.kr/Search/?stext=개발자&tabType=recruit"
-
-func main() {
-	not_trimed := getPages(mainURL)
-
-	// totalPageNum := getPages(mainURL)
-	// fmt.Println(totalPageNum)
-	fmt.Println(not_trimed)
+type extractedJob struct {
+	id			int		// Attr.data-gno
+	company		string	// div.post-list-corp > a.title
+	title		string	// div.post-list-info > a.title
+	career		string	// span.exp
+	location	string	// span.long
+	deadline	string	// span.date
 }
 
-func getPages(URL string) int {
-	pages := ""
+var mainURL string = "https://www.jobkorea.co.kr/Search/?stext=개발자&tabType=recruit"
 
-	res, err := http.Get(URL)
+func main() {
+	// lastP := getLastPages(mainURL)
+
+	// for i := 1; i <= lastP; i++{
+	// 	extractPage(i)
+	// }
+	extractPage(1)
+}
+
+func extractPage(page int) {
+	pageURL := mainURL + "&Page_No=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
 	checkErr(err)
 	checkCode(res)
 
@@ -31,16 +42,38 @@ func getPages(URL string) int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	pages = doc.Find("span.pgTotal").Text()
-	last_page := pages[0:3]
-	int_page, _ := strconv.Atoi(last_page)
-	// list := doc.Find(".tplPagination > ul.clear > li")
-	// list.Each(func(idx int, sel *goquery.Selection) {
-	// 	last_pages = sel.Find("span.pgTotdal").Text()
-	// })
-	return int_page
+	postList := doc.Find("li.list-post") // 20EA
+
+	postList.Each(func(i int, card *goquery.Selection) {
+		// id, _ := card.Attr("data-gno")
+		// company := strClnr(card.Find("div.post-list-corp > a.title").Text())
+		// title := strClnr(card.Find("div.post-list-info > a.title").Text())
+		// career := strClnr(card.Find("span.exp").Text())
+		// location := card.Find("span.long").Text()
+		// deadline := strClnr(card.Find("span.date").Text())
+		deadline := strClnr(card.Find("span.date").Text())
+		fmt.Println(deadline)
+	})
 }
 
+// func getLastPages(URL string) int {
+// 	pages := ""
+
+// 	res, err := http.Get(URL)
+// 	checkErr(err)
+// 	checkCode(res)
+
+// 	defer res.Body.Close()
+	
+// 	doc, err := goquery.NewDocumentFromReader(res.Body)
+// 	checkErr(err)
+
+// 	pages = doc.Find("span.pgTotal").Text()
+// 	lastPage := pages[0:3]
+// 	intPage, _ := strconv.Atoi(lastPage)
+	
+// 	return intPage
+// }
 
 func checkErr(err error) {
 	if err != nil {
@@ -52,4 +85,9 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status: ", res.StatusCode)
 	}
+}
+
+func strClnr(str string) []string { // stringCleaner
+	return strings.Fields(strings.TrimSpace(str))
+	//
 }

b0a017c15c800f7fcbd403be49d1415ac05a8515
Traceur0 <ghost01ghost@gmail.com>
2022-04-12 15:00:41 +0900

    go routine added

--- main.go
+++ main.go
@@ -26,17 +26,90 @@ var mainURL string = "https://www.jobkorea.co.kr/Search/?stext=개발자&tabType
 
 func main() {
 	var jobs []extractedJob
-	// lastP := getLastPages(mainURL)
+	c := make(chan []extractedJob)
+	lastP := getLastPages(mainURL)
 
-	// for i := 1; i <= lastP; i++{
-	// 	extractPage(i)
-	// }
-	extractedjobs := getPage(1)
-	jobs = append(jobs, extractedjobs...) // combine two array
+	for i := 1; i <= lastP; i++{
+		go getPage(i, c)
+	}
+
+	for i := 1; i <= lastP; i++{
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
 
 	writeJobs(jobs)
 	fmt.Println("Extraction Done", len(jobs))
 }
+
+
+func getPage(page int, mainChan chan<- []extractedJob) {
+	var jobs []extractedJob
+	c := make(chan extractedJob)
+	pageURL := mainURL + "&Page_No=" + strconv.Itoa(page)
+	fmt.Println("Requesting Page", strconv.Itoa(page))
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close() 
+	
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	postList := doc.Find("li.list-post") // 20EA -> 20(what i want)+20(dummy data)
+
+	postList.EachWithBreak(func(i int, card *goquery.Selection) bool {
+		company := strClnr(card.Find("div.post-list-corp > a").Text())
+		go extractPage(card, c)
+		
+		if company != "" {
+			return true
+		}
+		return false // End of EachWithBreak
+	})
+	for i := 1; i <= 20; i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	mainChan <- jobs
+}
+
+func extractPage(card *goquery.Selection, c chan<- extractedJob) {
+	id, _ := card.Attr("data-gno")
+	company := strClnr(card.Find("div.post-list-corp > a").Text())
+	location := card.Find("span.long").Text()
+	title := strClnr(card.Find("div.post-list-info > a.title").Text())
+	career := strClnr(card.Find("span.exp").Text())
+	deadline := strClnr(card.Find("p.option > span.date").Text())
+	c <- extractedJob{
+		id:			id,
+		company:	company,
+		location:	location,
+		title:		title,
+		career:		career,
+		deadline:	deadline}
+}
+
+func getLastPages(URL string) int {
+	pages := ""
+
+	res, err := http.Get(URL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	pages = doc.Find("span.pgTotal").Text()
+	lastPage := pages[0:3]
+	intPage, _ := strconv.Atoi(lastPage)
+	
+	return intPage
+}
+
 
 func writeJobs(jobs []extractedJob) {
 	file, err := os.Create("jobs.csv")
@@ -59,72 +132,6 @@ func writeJobs(jobs []extractedJob) {
 	}
 }
 
-func getPage(page int) []extractedJob {
-	var jobs []extractedJob
-
-	pageURL := mainURL + "&Page_No=" + strconv.Itoa(page)
-	fmt.Println("Requesting", pageURL)
-	res, err := http.Get(pageURL)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close() 
-	
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-
-	postList := doc.Find("li.list-post") // 20EA -> 20(what i want)+20(dummy data)
-
-	postList.EachWithBreak(func(i int, card *goquery.Selection) bool {
-		company := strClnr(card.Find("div.post-list-corp > a").Text())
-		
-		job := extractPage(card)
-		jobs = append(jobs, job)
-
-		if company != "" {
-			return true
-		}
-		return false // End of EachWithBreak
-	})
-	return jobs
-}
-
-func extractPage(card *goquery.Selection) extractedJob {
-	id, _ := card.Attr("data-gno")
-	company := strClnr(card.Find("div.post-list-corp > a").Text())
-	location := card.Find("span.long").Text()
-	title := strClnr(card.Find("div.post-list-info > a.title").Text())
-	career := strClnr(card.Find("span.exp").Text())
-	deadline := strClnr(card.Find("p.option > span.date").Text())
-	return extractedJob{
-		id:			id,
-		company:	company,
-		location:	location,
-		title:		title,
-		career:		career,
-		deadline:	deadline}
-}
-
-
-// func getLastPages(URL string) int {
-// 	pages := ""
-
-// 	res, err := http.Get(URL)
-// 	checkErr(err)
-// 	checkCode(res)
-
-// 	defer res.Body.Close()
-	
-// 	doc, err := goquery.NewDocumentFromReader(res.Body)
-// 	checkErr(err)
-
-// 	pages = doc.Find("span.pgTotal").Text()
-// 	lastPage := pages[0:3]
-// 	intPage, _ := strconv.Atoi(lastPage)
-	
-// 	return intPage
-// }
-
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
@@ -140,5 +147,3 @@ func checkCode(res *http.Response) {
 func strClnr(str string) string { // stringCleaner
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")// strings.fields() Deleted. // strings.join() Needed.
 }
-
-// 두가지 선택지
